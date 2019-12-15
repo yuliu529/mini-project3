4d2a107808339af013fa3c697b7e1b91757cbdbb
yuliu529 <55214270+yuliu529@users.noreply.github.com>
2019-12-15 00:05:41 -0500

    Create DSP.py

---
+++ DSP.py
@@ -0,0 +1,25 @@
+import IPython.display as ipd
+import librosa as lr
+import librosa.display
+import matplotlib.pyplot as plt
+
+def audio_vis(audio_path):
+    x, sr = lr.load(audio_path) #sr-sample rate
+    librosa.load(audio_path, sr=44100) #set sample rate
+    ipd.Audio(audio_path)
+    X = librosa.stft(x) #stft
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(14,5))
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    plt.figure(figsize = (14,5))
+    librosa.display.waveplot(x, sr = sr)
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar()
+    plt.show()
+
+if __name__ == "__main__":
+    audio_path = '/Users/liuyu/Desktop/weather.wav'
+    audio_vis(audio_path)
+
